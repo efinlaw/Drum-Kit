4c4187e057915610d45f1f27725ba865f76b3eed
efinlaw <47069204+efinlaw@users.noreply.github.com>
2023-05-18 15:45:17 -0700

    implemented keydown event to play sound and add class to key pressed

---
+++ index.js
@@ -0,0 +1,14 @@
+
+window.addEventListener('keydown', (e) => {
+    const {keyCode}= e;
+    const audio = document.querySelector(`audio[data-key='${keyCode}'`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    const key = document.querySelector(`div[data-key='${keyCode}']`);
+    key.classList.add('playing');
+});
+
+
+
